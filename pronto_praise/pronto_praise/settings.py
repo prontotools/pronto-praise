2b27333e0c250383ac7746144fca29a46198cdb7
zkan <kan@prontomarketing.com>
2017-09-06 12:23:48 +0700

    Configure settings for static files

--- pronto_praise/pronto_praise/settings.py
+++ pronto_praise/pronto_praise/settings.py
@@ -117,4 +117,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
+STATIC_ROOT = os.path.join(SITE_ROOT, 'staticfiles')
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS = (
+    os.path.join(SITE_ROOT, 'static'),
+)
