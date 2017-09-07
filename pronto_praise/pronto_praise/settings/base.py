15f88900e3721c0a7628bb2ef1ec7bd29725f28e
zkan <kan@prontomarketing.com>
2017-09-07 07:17:44 +0700

    Use base template for each page :bear:

--- pronto_praise/pronto_praise/settings/base.py
+++ pronto_praise/pronto_praise/settings/base.py
@@ -56,7 +56,7 @@ ROOT_URLCONF = 'pronto_praise.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [SITE_ROOT + '/templates/'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
