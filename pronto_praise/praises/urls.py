e660d5d33339ec755f5ab3c195d15ae968ee35e2
Mils <mils@PRONTOLAP0050.local>
2017-09-09 13:46:50 +0700

    Add PraiseAddHeart

--- pronto_praise/praises/urls.py
+++ pronto_praise/praises/urls.py
@@ -3,10 +3,12 @@ from django.conf.urls import url
 from .views import (
     PraiseListView,
     PraiseAddView,
+    PraiseAddHeart
 )
 
 
 urlpatterns = [
     url(r'^$', PraiseListView.as_view(), name='praise_list'),
     url(r'^add/$', PraiseAddView.as_view(), name='praise_add'),
+    url(r'^(?P<praise_id>\d+)/heart/$', PraiseAddHeart.as_view(), name='praise_add_heart'),
 ]
